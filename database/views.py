27d9c64244f6d8346b430f692673c9a6350596ae
Sal-Kal <saishkalbag@gmail.com>
2022-09-04 21:14:33 +0530

    heroku

--- database/views.py
+++ database/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django. http import HttpResponse, JsonResponse
 from .models import detail
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    try:
+        x = detail.objects.get(key = "Geography")
+        if x:
+            return render(request, 'index.html')
+
+    except Exception as e:
+        response = redirect('http://localhost:8000/populate')
+        return response
 
 def getDetails(request, key):
     try:
@@ -21,3 +28,20 @@ def getDetails(request, key):
             "message" : str(e)
         })
 
+def populate(requet):
+    try:
+        new_detail = detail(key = "Geography", value = "The Geography of Asgard is dominated by huge mountains.")
+        new_detail.save()
+        new_detail = detail(key = "History", value = "History of Asgard goes back a millenia.")
+        new_detail.save()
+        new_detail = detail(key = "Culture", value = "Culturally Asgard remains a mystery to the passer by.")
+        new_detail.save()
+        new_detail = detail(key = "Language", value = "Language barriers in Asgard do not allow outsiders to live.")
+        new_detail.save()
+        response = redirect('http://localhost:8000/')
+        return response
+    except Exception as e:
+        return JsonResponse({
+            "status" : "error",
+            "message" : str(e)
+        })
